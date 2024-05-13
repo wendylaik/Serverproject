914ba531525003cc5bd7bacf463ee8c013bd8cf4
Jordy <Jordy vindas>
2024-05-12 19:09:37 -0600

    Aregle un error con el cierre de una ventana del register

--- Server/src/Interfaz/Logiin.java
+++ Server/src/Interfaz/Logiin.java
@@ -156,6 +156,7 @@ public class Logiin extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.dispose();
         JFrame frame = new JFrame("Registro");
         Register registerPanel = new Register();
         frame.add(registerPanel);
